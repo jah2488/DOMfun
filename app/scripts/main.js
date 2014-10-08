fa5cfa34a687f9987cff8e2fadd71ff48d3a5cba
josefv <josefv@users.noreply.github.com>
2014-10-07 22:58:30 -0400

    done w part 1 and 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,58 +1,37 @@
 
 /**********************
-PART 1
+PART I
 **********************/
 
-// #1
+// Step 1
 
 var headerElem = document.body.children[0];
 
-// headerElem.innerHTML;
+headerElem.style.color = 'blue';
 
-// console.log(headerElem.innerHTML);
-
-headerElem.style.color = 'red';
-
-// console.log(headerElem.style.color);
-
-// #2
+// Step 2
 
 var middleEleye = document.getElementsByTagName('li')[1];
 console.log(middleEleye.innerHTML);
 
-// var eleye = document.querySelector('li');
+// var eleye = document.querySelectorAll('li')[1];
 // console.log(eleye);
 
-// #3
+// Step 3
 
-// var newElm = document.createElement('h2');
-// var mainSec = document.getElementById('main');
+var newElm = document.createElement('h2');
+var mainSec = document.getElementById('main');
 
-// mainSec.appendChild(newElm);
+mainSec.appendChild(newElm);
 
 /*****************
-PART 2
+PART II
 *****************/
 
-// var templateString = document.getElementById('nameTemplate').innerHTML;
+/* example of using template, then 'find and replace' element */
+var templateString = document.getElementById('nameTemplate').innerHTML; // no '#' needed ???
 
-// var nameTemplate = _.template(templateString); //fn
-
-// var compiledTemplate = nameTemplate({
-// 	name: 'bill'
-// });
-
-// console.log(templateString);
-// console.log(compiledTemplate);
-
-// document.getElementById('main');
-
-/**********************
-
-
-var templateString = document.getElementById('nameTemplate').innerHTML;
-
-var nameTemplate = _.template(templateString);
+var nameTemplate = _.template(templateString); // this is a function!
 
 
 // My variable of names.
@@ -60,55 +39,48 @@ var names = [
   'bill',
   'jane',
   'sally',
-  'mary'
+  'mary',
+  '<hr>'
 ];
 
-//Getting a list of compiled templates by mapping over my list of names.
+// Getting a list of compiled templates by mapping over my list of names.
 var nameTemplates = _.map(names, function (personName) {
-  return nameTemplate({ name: personName });// Notice that an object is being passed into this function
-                                            // this object has the key that matches my template in HTML.
-        //I'm calling the nameTemplate function once for each name and returning it.
-});
+  return nameTemplate({ name: personName });
+}); 		// Notice that an object is being passed into this function
+        // this object has the key (oooohhhhh!) that matches my template in HTML.
+        // I'm calling the nameTemplate function once for each name and returning it.
 
-var destination = document.querySelector('#nameTarget');
+var destination = document.querySelector('#nameTarget'); // query selector ..have to use '#'' ???
 //Grab the element where I want to put the data
 
 destination.innerHTML = nameTemplates.join('');
 //Assign the innerHTML of my destination element to all the name templates joined together.
 
 
+/* my example */
+var elementString = document.getElementById('colorTemplate').innerHTML;
 
+var colorTemplate = _.template(elementString); // fn
 
-var templateNewString = document.getElementById('ageTemplate').innerHTML;
+var colors = [
+	'red',
+	'green',
+	'blue',
+	'wat',
+	'one',
+	'WatMan',
+	'oh wait these were supposed to be colors'
+];
 
-var ageTemplate = _.template(templateNewString);
-
-var addedTemplate = ageTemplate({
-	age: 28
+var forColors = _.map(colors, function (colorName) {
+	return colorTemplate({color: colorName});
 });
 
-console.log(templateNewString);
-console.log(addedTemplate);
+// var dest = document.getElementById('myTarget'); // this line does the same thing as the line below..so i'm
+																										// guessing that answers my # question?
+var dest = document.querySelector('#myTarget'); // querySelector with the #
 
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
+dest.innerHTML = forColors.join('');
 
 
 
