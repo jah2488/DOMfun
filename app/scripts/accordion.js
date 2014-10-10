c25e4847637720d1c5860a05e381e4a3708ed29f
Justin Herrick <justin@justinherrick.com>
2014-10-10 18:26:41 -0400

    Update accordion.js

---
+++ app/scripts/accordion.js
@@ -0,0 +1,6 @@
+//This file seems to be lacking slightly.
+/*
+
+Go ahead and finish this assignment. Creating an according in css and then use jQuery to add/remove css classes to
+manage the css transitions.
+*/
